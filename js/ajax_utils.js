16f664fd27c19e0c23eb47d496503eeb95fc5937
zziamalei <821718601@qq.com>
2018-04-27 16:11:49 +0800

    角色列表

--- js/ajax_utils.js
+++ js/ajax_utils.js
@@ -69,6 +69,9 @@ $.extend({
             type: options.type,
             url: apiRoot+options.url,
             data: options.data,
+            beforeSend: function(request) {
+                request.setRequestHeader("access_token", "1");
+            },
             success: function(data){
                var  backData=JSON.parse(data);
                 if(backData.code=='0'){
@@ -81,4 +84,195 @@ $.extend({
         });
 
     }
-})
+});
+
+
+
+$.extend({
+    myList: function(options) {
+        var _default = {
+            id : 'table',
+            url : '',
+            cols:[],
+            successBack : function(){},
+            failBack:function () {
+
+            },
+            setlayerTable:function () {
+
+            }
+        };
+
+        var options=jQuery.extend({},_default,options);
+
+
+
+        var table;
+        layui.use('table', function(){
+            table= layui.table;
+
+            options.setlayerTable(table);
+
+            //第一个实例
+            table.render({
+                elem: '#'+options.id
+                // ,height: 315
+                ,url:apiRoot+ options.url //数据接口
+                ,headers: {access_token: '1'}
+                ,page: true //开启分页
+                ,request: {
+                    pageName: 'pageNum' //页码的参数名称，默认：page
+                    ,limitName: 'pageSize' //每页数据量的参数名，默认：limit
+                }
+                ,response: {
+                    statusName: 'code' //数据状态的字段名称，默认：code
+                    ,statusCode: 0 //成功的状态码，默认：0
+                    ,msgName: 'msg' //状态信息的字段名称，默认：msg
+                }
+                ,cols: [options.cols]
+            });
+
+
+
+            if(options.tool){
+
+                //监听工具条
+                table.on('tool('+options.id+')', function(obj){ //注：tool是工具条事件名，test是table原始容器的属性 lay-filter="对应的值"
+                    var data = obj.data; //获得当前行数据
+                    var layEvent = obj.event; //获得 lay-event 对应的值（也可以是表头的 event 参数对应的值）
+                    var tr = obj.tr; //获得当前行 tr 的DOM对象
+
+                    var areaWidth='700px';
+                    var areaHeight='400px';
+
+                    if($('a[lay-event='+layEvent+']').attr('areaWidth')){
+                        areaWidth=$('a[lay-event='+layEvent+']').attr('areaWidth');
+                    }
+                    if($('a[lay-event='+layEvent+']').attr('areaHeight')){
+                        areaHeight=$('a[lay-event='+layEvent+']').attr('areaHeight');
+                    }
+
+
+
+                    if(layEvent === 'open'||layEvent === 'open1'||layEvent === 'open2'||layEvent === 'open3'||layEvent === 'open4'){ //查看
+
+                        layer.open({
+                            type: 2,
+                            area: [areaWidth, areaHeight],
+                            fixed: false, //不固定
+                            maxmin: true,
+                            content: $('a[lay-event='+layEvent+']').attr('url')+'?'+$('a[lay-event='+layEvent+']').attr('idName')+'='+data[$('a[lay-event='+layEvent+']').attr('idName')]
+                        });
+
+                    } else if(layEvent === 'confirm'){ //删除
+                        layer.confirm('真的删除行么', function(index){
+                            obj.del(); //删除对应行（tr）的DOM结构，并更新缓存
+                            layer.close(index);
+                            //向服务端发送删除指令
+
+                        });
+                    }
+
+
+
+                });
+
+            }
+
+
+
+        });
+
+
+        if(options.layer){
+            options.layer=layer;
+        }
+        if(options.table){
+            options.table=table;
+        }
+    }
+});
+
+
+$.extend({
+    myEdit:function (options) {
+        var _default = {
+            title : '',
+            id : 'uid',
+            url : '',
+            successBack : function(){},
+            failBack:function () {
+
+            },
+            setForm:function () {
+
+            }
+        };
+
+        var options=jQuery.extend({},_default,options);
+
+        if(options.id){
+            parent.$('.layui-layer-title').html('编辑'+options.title);
+        }else{
+            parent.$('.layui-layer-title').html('新增'+options.title);
+        }
+
+        var index = parent.layer.getFrameIndex(window.name); //获取窗口索引
+
+        $('.close-btn').click(function () {
+            parent.layer.close(index);
+
+        });
+
+        var form
+        layui.use(['form', 'layedit', 'laydate'], function(){
+            form = layui.form
+                ,layer = layui.layer
+                ,layedit = layui.layedit
+                ,laydate = layui.laydate;
+
+
+            //自定义验证规则
+            form.verify({
+                title: function(value){
+                    if(value.length < 5){
+                        return '标题至少得5个字符啊';
+                    }
+                }
+                ,pass: [/(.+){6,12}$/, '密码必须6到12位']
+                ,content: function(value){
+                    layedit.sync(editIndex);
+                }
+            });
+
+            //监听提交
+            form.on('submit(save)', function(data){
+                layer.alert(JSON.stringify(data.field), {
+                    title: '最终的提交信息'
+                });
+
+
+                $.myAjax({
+                    url:$('.layui-form').attr('action'),
+                    data:data.field,
+                    type:'post',
+                    successBack:function (msg) {
+                        layer.msg(msg, {icon: 1,time:1000},function () {
+                            parent.table.reload('table');
+
+                            parent.layer.close(index);
+                        });
+                    },
+                    failBack:function (code,msg) {
+                        layer.msg(msg, {icon: 2,time:2000});
+                    }
+
+                });
+
+                return false;
+            });
+
+
+        });
+    }
+});
